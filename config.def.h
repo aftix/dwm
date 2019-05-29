5bccd7e318950c4cda2260c90af86054b2f75c58
aftix <gameraftexploision@gmail.com>
2019-05-28 21:50:29 -0400

    Make feh float

--- config.def.h
+++ config.def.h
@@ -31,6 +31,8 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,                  NULL,            0,       1,           -1 },
 	{ "Firefox",  NULL,                  NULL,       1 << 8,       0,           -1 },
   { "Firefox",  NULL, "Firefox Preferences",       1 << 8,       1,           -1 },
+  { "Conky",    NULL,                  NULL,            0,       1,           -1 },
+  { "feh",      NULL,                  NULL,            0,       1,           -1 },
 };
 
 /* layout(s) */
